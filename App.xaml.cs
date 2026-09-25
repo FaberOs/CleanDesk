60265c8d3e8e28fd09e5e028a82307f0b96e7a8c
FaberOs <fospina@unicauca.edu.co>
2026-09-25 12:07:46 -0500

    feat(displays): add DisplayManager with multi-monitor profiles and MultiMonitorTool integration

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,22 @@
+using System;
+using System.IO;
 using System.Windows;
 
 namespace CleanDesk
 {
     public partial class App : Application
     {
+        public App()
+        {
+            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+            {
+                try { File.WriteAllText(Path.Combine(Path.GetTempPath(), "cleandesk_crash.log"), e.ExceptionObject.ToString()); } catch { }
+            };
+            this.DispatcherUnhandledException += (s, e) =>
+            {
+                try { File.WriteAllText(Path.Combine(Path.GetTempPath(), "cleandesk_crash.log"), e.Exception.ToString()); } catch { }
+            };
+        }
+
     }
 }
